a465a57138841a874042b2a01ea2f9ab40c81276
luiznds62 <luiznds62@gmail.com>
2021-03-05 15:36:23 -0300

    User repository test created

---
+++ tests/domain/UserRepository.test.ts
@@ -0,0 +1,11 @@
+import nedb from 'nedb';
+import { User } from '../../src/domain/user/User';
+import { UserRepository } from '../../src/domain/user/UserRepository';
+
+describe('UserRepository', () => {
+  test('Should create instance', async () => {
+    const repository: UserRepository = new UserRepository();
+
+    expect(repository).toBeDefined();
+  });
+});
---
+++ tests/domain/UserTestUtils.ts
@@ -0,0 +1,16 @@
+export const USER_CONSTS = {
+  email: 'test@hotmail.com',
+  pagination: {
+    offset: 0,
+    limit: 20,
+    sort: ''
+  },
+  userProps: {
+    _id: '123456',
+    name: 'testes',
+    email: 'test@hotmail.com',
+    password: '12345678'
+  },
+  changedUser: 'Changed user',
+  affecteds: 1
+};
